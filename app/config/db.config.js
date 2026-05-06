16696f3b48ea27df6e260b96989248da4ca8c907
Lizbeth Chambi Luque <gleny.chambi@tecsup.edu.pe>
2026-05-06 13:34:54 -0500

    Configura variables de entorno para MySQL

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,15 +1,8 @@
-// app/config/db.config.js
 export default {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "1234",
-  DB: "login_db",
-  PORT: 3306,
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
 };
